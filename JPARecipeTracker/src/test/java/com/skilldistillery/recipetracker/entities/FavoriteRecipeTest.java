25559a0c18ef5ccd2db2483baaab898efe9dd92a
Frederick Kaucher <frederick.kaucher@gmail.com>
2022-01-26 15:12:11 -0700

    test entity mapping for favoriteRecipe

--- JPARecipeTracker/src/test/java/com/skilldistillery/recipetracker/entities/FavoriteRecipeTest.java
+++ JPARecipeTracker/src/test/java/com/skilldistillery/recipetracker/entities/FavoriteRecipeTest.java
@@ -2,15 +2,6 @@ package com.skilldistillery.recipetracker.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,17 +9,20 @@ class FavoriteRecipeTest extends TestTemplate {
 
 	private FavoriteRecipe fr;
 
+	
 	@Override
 	void setUp() throws Exception {
-		fr = em.find(FavoriteRecipe.class, 1);	
+		fr = em.find(FavoriteRecipe.class, new UserHasFavoriteRecipeId(1,1));	
 	}
 
 	@Test
 	void test_User_entity_mapping() {
 		assertNotNull(fr);
-		assertEquals("Wolfgang love’s this lasangna and want’s to favorite it!", fr.getComment());
-		assertEquals("2022-01-01T00:00:00", fr.getCreatedAt());
-		assertEquals("2022-01-01", fr.getDateLastMade());
+		assertEquals("Wolfgang love's this lasangna and want's to favorite it!", fr.getComment());
+		assertEquals(2022, fr.getCreatedAt().getYear());
+		assertEquals(01, fr.getDateLastMade().getMonthValue());
+		
+		
 		
 	}
 
