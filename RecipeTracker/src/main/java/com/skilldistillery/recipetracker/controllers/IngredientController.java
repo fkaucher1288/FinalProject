e6a2d13ff04df81f7d0212270f769ba30249ffc2
thedaneeffect <github@dane.fyi>
2022-01-27 11:29:42 -0700

    Added Ingredients Controller/Repository/API

---
+++ RecipeTracker/src/main/java/com/skilldistillery/recipetracker/controllers/IngredientController.java
@@ -0,0 +1,27 @@
+package com.skilldistillery.recipetracker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.recipetracker.entities.Ingredient;
+import com.skilldistillery.recipetracker.services.IngredientService;
+
+@RequestMapping("api/ingredients")
+@CrossOrigin({"*", "http://localhost:4200"})
+@RestController
+public class IngredientController {
+
+	@Autowired
+	private IngredientService service;
+	
+	@GetMapping
+	public List<Ingredient> index() {
+		return service.getAll();
+	}
+	
+}
