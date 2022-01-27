f630edac9229bfd908baa77cad383d50aadaa245
Panthro22 <jordonpaynter22@gmail.com>
2022-01-27 12:23:06 -0700

    Finished Repository, Service, and Controller for DietPlan. Also tested on postman successful

--- RecipeTracker/src/main/java/com/skilldistillery/recipetracker/controllers/DietPlanController.java
+++ RecipeTracker/src/main/java/com/skilldistillery/recipetracker/controllers/DietPlanController.java
@@ -1,21 +1,28 @@
 package com.skilldistillery.recipetracker.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.recipetracker.entities.DietPlan;
+import com.skilldistillery.recipetracker.entities.DietPlanIngredient;
+import com.skilldistillery.recipetracker.entities.DietPlanRecipe;
+import com.skilldistillery.recipetracker.entities.User;
+import com.skilldistillery.recipetracker.services.AuthService;
 import com.skilldistillery.recipetracker.services.DietPlanService;
 
 @RestController
@@ -25,6 +32,9 @@ public class DietPlanController {
 	
 	@Autowired
 	private DietPlanService dPlanSvc;
+	
+	@Autowired
+	private AuthService authSvc;
 	
 	@GetMapping("dietplans")
 	public List<DietPlan> index(){
@@ -39,22 +49,35 @@ public class DietPlanController {
 		}
 		return dietPlan;
 	}
-	
+	@GetMapping("dietplans/{dietPlanId}/ingredients")
+	public List<DietPlanIngredient> getIngredients(@PathVariable int dietPlanId, HttpServletResponse res, Principal principal){
+		DietPlan dPlan = dPlanSvc.findDietPlanById(dietPlanId);
+		return dPlan.getIngredients();
+	}
+	@GetMapping("dietplans/{dietPlanId}/recipes")
+	public List<DietPlanRecipe> getDietPlanRecipes(@PathVariable int dietPlanId, HttpServletResponse res, Principal principal){
+		DietPlan dPlan = dPlanSvc.findDietPlanById(dietPlanId);
+		return dPlan.getRecipes();
+	}
 	@PostMapping("dietplans")
-	public DietPlan createDietPlan(@RequestBody DietPlan dietPlan, HttpServletResponse res, HttpServletRequest req) {
-		try {
-			dPlanSvc.createDietPlan(dietPlan);
-			res.setStatus(201);
-			StringBuffer url = req.getRequestURL();
-			url.append("/").append(dietPlan.getId());
-			res.setHeader("Location", url.toString());
-		} catch(Exception e) {
-			e.printStackTrace();
-			System.err.println("INVALID JSON FOR NEW DietPlan");
-			res.setStatus(400);
-			dietPlan = null;
+	public DietPlan createDietPlan(@RequestBody DietPlan dietPlan, HttpServletResponse res, HttpServletRequest req, Principal principal){
+		User user = authSvc.findUserByName(principal.getName());
+		dietPlan.setUser(user);
+		return 	dPlanSvc.createDietPlan(dietPlan);
+	}
+	
+	@PutMapping("dietplans")
+	public DietPlan updateDietPlan(@RequestBody DietPlan dietPlan, HttpServletResponse res, HttpServletRequest req, Principal principal) throws Exception{
+		User user = authSvc.findUserByName(principal.getName());
+		List<DietPlan> dietPlans = user.getDietPlans();
+		for(DietPlan plan: dietPlans) {
+			if(plan.equals(dietPlan)) {
+				dietPlan.setUser(user);
+				return dPlanSvc.updateDietPlan(dietPlan);
+			}
 		}
-		return dietPlan;
+		res.sendError(HttpStatus.NOT_FOUND.value());
+		return null;
 	}
 	
 	@DeleteMapping("dietplans/{dietPlanId}")
