15d5bd28c0eab5971dc21b4eea6076f557530630
Frederick Kaucher <frederick.kaucher@gmail.com>
2022-01-29 15:14:26 -0700

    recipe search page working, needs completed

--- ngRecipeTracker/src/app/components/recipe-search/recipe-search.component.ts
+++ ngRecipeTracker/src/app/components/recipe-search/recipe-search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { Ingredient } from 'src/app/models/ingredient';
+import { Recipe } from 'src/app/models/recipe';
+import { CategoryService } from 'src/app/services/category.service';
+import { IngredientService } from 'src/app/services/ingredient.service';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-search',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeSearchComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  recipes: Recipe[] = [];
+  ingredients: Ingredient[] = [];
+  filter: string = "";
+
+
+
+  constructor(
+   private categorySvc: CategoryService,
+   private recipeSvc: RecipeService,
+   private ingredientSvc: IngredientService
+  ) { }
 
   ngOnInit(): void {
+
+    this.categorySvc.index().subscribe(result => {
+
+      this.categories = result;
+
+    })
+
+    this.ingredientSvc.index().subscribe(result => {
+      this.ingredients = result;
+    })
+
   }
 
 }
