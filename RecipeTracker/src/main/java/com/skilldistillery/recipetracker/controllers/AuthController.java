b0194c5242ee0f9619e0163123a5bf72ddcdde28
Panthro22 <jordonpaynter22@gmail.com>
2022-01-31 13:37:17 -0700

    Added in Auth service in STS to return user by Username

--- RecipeTracker/src/main/java/com/skilldistillery/recipetracker/controllers/AuthController.java
+++ RecipeTracker/src/main/java/com/skilldistillery/recipetracker/controllers/AuthController.java
@@ -5,10 +5,10 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +25,8 @@ public class AuthController {
 
 	// TEMPORARY, DELETE LATER
 	@GetMapping("usertest")
-	public User userTest() {
-		return authSvc.findUserByName("wolfgangPuck");
+	public User getUserByUsernamew(@PathVariable String Username) {
+		return authSvc.findUserByName("Username");
 	}
 
 	@PutMapping("/register")
