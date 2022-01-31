4c5a8eef303fe3d5e27b40f05b03150d55ec2c02
Frederick Kaucher <frederick.kaucher@gmail.com>
2022-01-31 11:34:47 -0700

    added add recipe page

--- ngRecipeTracker/src/app/components/profile/profile.component.ts
+++ ngRecipeTracker/src/app/components/profile/profile.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Recipe } from 'src/app/models/recipe';
+import { AuthService } from 'src/app/services/auth.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { CookbookService } from 'src/app/services/cookbook.service';
+import { IngredientService } from 'src/app/services/ingredient.service';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +15,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('recipeModalTemplate', { read: TemplateRef })
+  recipeModalTemplate!: TemplateRef<any>;
+
+  addRecipe: boolean = false;
+
+  recipe: Recipe = {
+
+    id: 0,
+    name: '',
+    imageURL: '',
+    active: true,
+    creatorId: 0,
+    isPublic: true,
+    prepTime: '',
+    cookTime: '',
+    description: "",
+    instructions: "",
+    notes: "",
+    ingredients: [],
+    categories: []
+
+
+  }
+
+  constructor(private recipeService: RecipeService, private cookbookService: CookbookService, private ingredientService: IngredientService, private categoryService: CategoryService, private modal: NgbModal, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  onClickAddRecipe() {
+    this.modal.open(this.recipeModalTemplate).result.then(
+      (reason) => {
+        console.log('close reason:', reason);
+
+        if (reason === 'add recipe') {
+          this.recipeService
+            .create(this.recipe)
+            .subscribe({
+              next: (result) => {
+                console.log('recipe added', result);
+              },
+              error: (error) => {
+                console.log('error adding recipe', error);
+              }
+            });
+        }
+      })
+  }
+
 
 }
